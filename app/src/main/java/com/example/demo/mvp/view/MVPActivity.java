72985306838c92b156b6ad82a1037d0837a596ea
ws <sohozhou@163.com>
2018-10-09 10:23:16 +0800

    mvp first commit

---
+++ app/src/main/java/com/example/demo/mvp/view/MVPActivity.java
@@ -0,0 +1,14 @@
+package com.example.demo.mvp.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.demo.myapplication.R;
+
+public class MVPActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
