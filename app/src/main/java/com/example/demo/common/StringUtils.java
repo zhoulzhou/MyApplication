6e22e834179e6386d8cdd29846b1cc35a79b317f
ws <sohozhou@163.com>
2018-10-15 09:31:13 +0800

    修改字符串判空的条件

--- app/src/main/java/com/example/demo/common/StringUtils.java
+++ app/src/main/java/com/example/demo/common/StringUtils.java
@@ -3,7 +3,7 @@ package com.example.demo.common;
 public class StringUtils {
 
     public static boolean isEmpty(String string){
-        if(string == null || string.isEmpty()){
+        if(string == null || string.length() <=0){
             return true;
         }
         return false;
