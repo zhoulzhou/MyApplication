0c04ff130900f48904d5a39a48d1776158a17b13
ws <sohozhou@163.com>
2018-10-15 17:26:04 +0800

    二阶贝塞尔曲线 起点 控制点 终点 quadto rquadto 是相对的位置  都是相对于上一个的终点 起点都是通过path.moveto 确定

--- app/src/main/java/com/example/demo/customview/circleprogressview/CircleProgressView.java
+++ app/src/main/java/com/example/demo/customview/circleprogressview/CircleProgressView.java
@@ -2,8 +2,12 @@ package com.example.demo.customview.circleprogressview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -20,6 +24,9 @@ public class CircleProgressView extends View {
     private float circleRadius;
     private Paint textPaint;
     private Paint circlePaint;
+    private Paint progressPaint;
+    private Path path;
+    private int progressColor;
     private int progress;
 
     public CircleProgressView(Context context, @Nullable AttributeSet attrs) {
@@ -39,6 +46,7 @@ public class CircleProgressView extends View {
         centerTextColor = typedArray.getColor(R.styleable.circleprogressview_centerTextColor, 0xffffff);
         circleColor = typedArray.getColor(R.styleable.circleprogressview_circleColor, 0xff4081);
         circleRadius = typedArray.getDimension(R.styleable.circleprogressview_circleRadius, 260f);
+        progressColor = typedArray.getColor(R.styleable.circleprogressview_progressColor,0x3F51B5);
         typedArray.recycle();
 
 //        System.out.println("centerText= " + centerText);
@@ -57,6 +65,14 @@ public class CircleProgressView extends View {
         circlePaint.setColor(circleColor);
         circlePaint.setAntiAlias(true);
 
+        progressPaint = new Paint();
+        progressPaint.setColor(progressColor);
+        progressPaint.setAntiAlias(true);
+        //取两层绘制交集。显示上层
+        progressPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+
+        path = new Path();
+
     }
 
     @Override
@@ -70,12 +86,48 @@ public class CircleProgressView extends View {
 
         canvas.drawCircle(width / 2, height / 2, circleRadius, circlePaint);
 
+        //将text画在圆心位置
         float textWidth = textPaint.measureText(centerText);
         float textX = width / 2 - textWidth / 2;
         Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
         float dy = -(fontMetrics.ascent + fontMetrics.descent) / 2;
         float textY = height / 2 + dy;
         canvas.drawText(centerText, textX, textY, textPaint);
+
+        Bitmap bitmap = Bitmap.createBitmap((int) circleRadius*2, (int) circleRadius*2,Bitmap.Config.ARGB_8888);
+        Canvas bitmapCanvas = new Canvas(bitmap);
+        bitmapCanvas.drawCircle(width/2,height/2, circleRadius,circlePaint);
+
+        float waveY = (float) (1-progress*0.01) *circleRadius*2;
+
+        path.moveTo(width-circleRadius*2,0);
+        path.lineTo(0,height);
+        path.lineTo(width/2,height);
+
+//        path.moveTo(width,waveY);
+//        path.lineTo(width,height);
+//        path.lineTo(0,height);
+//        path.lineTo(0,waveY);
+
+//        float space = (circleRadius * 2)/ 6;
+//        float space = 30;
+//        float d = (float) (1-progress*0.01) *space;
+//
+//        System.out.println("circleRadius= " + circleRadius);
+//        System.out.println("progress= " + progress);
+        System.out.println("waveY= " + waveY);
+        System.out.println("width= " + width);
+        System.out.println("height= " + height);
+//        System.out.println("space= " + space);
+//        System.out.println("d= " + d);
+//
+//        for(int i=0; i<circleRadius/(space*4); i++){
+//            path.rQuadTo(space, -d,space*2, 0);
+//            path.rQuadTo(space,d,space*2,0);
+//        }
+
+        path.close();
+        bitmapCanvas.drawPath(path,progressPaint);
     }
 
     @Override
@@ -116,6 +168,18 @@ public class CircleProgressView extends View {
         invalidate();
     }
 
+    /**
+     * 这里重绘控件使用的是postInvalidate();而我们以前也有用Invalidate()函数的。
+     * 这两个函数的作用都是用来重绘控件的，但区别是Invalidate()一定要在UI线程执行，
+     * 如果不是在UI线程就会报错。而postInvalidate()则没有那么多讲究，它可以在任何线程中执行，
+     * 而不必一定要是主线程。其实在postInvalidate()就是利用handler给主线程发送刷新界面的消息来实现的，
+     * 所以它是可以在任何线程中执行，而不会出错。而正是因为它是通过发消息来实现的，
+     * 所以它的界面刷新可能没有直接调Invalidate()刷的那么快。
+
+     所以在我们确定当前线程是主线程的情况下，还是以invalide()函数为主。
+     当我们不确定当前要刷新页面的位置所处的线程是不是主线程的时候，还是用postInvalidate为好;
+     *
+     */
     public void setProgress(int progress){
         this.progress = progress;
         this.centerText = progress + "%";
