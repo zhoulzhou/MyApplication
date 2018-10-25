606dac5cf100c4a4be84e512d0eaf44a1eb35bcc
ws <sohozhou@163.com>
2018-10-25 14:33:36 +0800

    位移运算 a << n a 左移 n位

--- app/src/main/java/com/example/demo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/demo/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.demo.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.demo.designpattern.strategypattern.Price;
+
 public class MainActivity extends AppCompatActivity{
 
     @Override
@@ -10,6 +12,18 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        test();
+    }
+
+    private void test(){
+        int COUNT_BITS = Integer.SIZE - 3;
+        System.out.println("COUNT_BITS= " + COUNT_BITS + " == " + Integer.toBinaryString(COUNT_BITS));
+
+        int r = -1 << COUNT_BITS;
+        System.out.println("r= " + r + " == " + Integer.toBinaryString(r));
+
+        int s = 1 << COUNT_BITS;
+        System.out.println("s= " + s + " == " + Integer.toBinaryString(s));
     }
 
 
