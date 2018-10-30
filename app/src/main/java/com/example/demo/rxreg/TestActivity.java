198e6575b376377c8c5d0ba1ad1c19b3d4a0f448
ws <sohozhou@163.com>
2018-10-30 14:31:54 +0800

    换新的测试文件

---
+++ app/src/main/java/com/example/demo/rxreg/TestActivity.java
@@ -0,0 +1,17 @@
+package com.example.demo.rxreg;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.demo.myapplication.R;
+
+public class TestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
