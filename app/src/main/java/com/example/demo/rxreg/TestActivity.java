e6e1cbdf6d35c9412a5878eef2e4d5928961fb9f
ws <sohozhou@163.com>
2018-10-30 15:29:34 +0800

    建立json bean

--- app/src/main/java/com/example/demo/rxreg/TestActivity.java
+++ app/src/main/java/com/example/demo/rxreg/TestActivity.java
@@ -3,14 +3,25 @@ package com.example.demo.rxreg;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.example.demo.myapplication.R;
 
+import org.w3c.dom.Text;
+
 public class TestActivity extends AppCompatActivity {
+    private TextView mTextView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.text);
+
+    }
+
+    private void testRxjava(){
+
     }
 
 
---
+++ app/src/main/java/com/example/demo/rxreg/Translation.java
@@ -0,0 +1,43 @@
+package com.example.demo.rxreg;
+
+/**
+ * // URL模板
+ http://fy.iciba.com/ajax.php
+
+ // URL实例
+ http://fy.iciba.com/ajax.php?a=fy&f=auto&t=auto&w=hello%20world
+
+ // 参数说明：
+ // a：固定值 fy
+ // f：原文内容类型，日语取 ja，中文取 zh，英语取 en，韩语取 ko，德语取 de，西班牙语取 es，法语取 fr，自动则取 auto
+ // t：译文内容类型，日语取 ja，中文取 zh，英语取 en，韩语取 ko，德语取 de，西班牙语取 es，法语取 fr，自动则取 auto
+ // w：查询内容
+
+ 返回的GSON格式
+ {
+   "status": 1
+   "content"：{
+      “from": "en-EU"
+      "to": "zh-CN"
+      "vendor": "baidu"
+      "out": "你好世界<br/>"
+      "err_no": 0
+    }
+ }
+ */
+public class Translation {
+    private int status;
+    private Content content;
+
+    public void show(){
+
+    }
+
+    private static class Content{
+        private String from;
+        private String to;
+        private String vendor;
+        private String out;
+        private int errNo;
+    }
+}
